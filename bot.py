cd71f34c294a3133fdf2b43639fb505a24526a4c
tonycai777 <tonycai777@icloud.com>
2021-10-18 16:53:46 -0500

    Add error handling

--- bot.py
+++ bot.py
@@ -16,5 +16,6 @@ bot.load_extension("pingpong")
 bot.load_extension("blackjack")
 bot.load_extension("ytsearch")
 bot.load_extension("weather")
+bot.load_extension("ErrorHandler")
 
 bot.run("ODk4MzgzMDEyMzQ0MDA0NjE5.YWjaYQ.3_e2ehvAZNGJLRnR2BaxnUc10JM")
