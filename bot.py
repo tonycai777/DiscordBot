c8cec919433b974a0fe4787ec48bcbb727cac32d
tonycai777 <tonycai777@icloud.com>
2021-10-18 10:07:40 -0500

    adding yt search feature

--- bot.py
+++ bot.py
@@ -4,5 +4,6 @@ bot = commands.Bot(command_prefix="\\")
 
 bot.load_extension("pingpong")
 bot.load_extension("blackjack")
+bot.load_extension("ytsearch")
 
 bot.run("ODk4MzgzMDEyMzQ0MDA0NjE5.YWjaYQ.3_e2ehvAZNGJLRnR2BaxnUc10JM")
---
+++ ytsearch.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+import asyncio
+import urllib.request
+import re
+
+class ytsearch(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name="video")
+    async def ping(self, ctx: commands.Context):
+        #Searches for yt video!
+
+        message = await ctx.send("Enter a search term")
+        check = lambda m: m.author == ctx.author and m.channel == ctx.channel
+
+        try:
+            confirm = await self.bot.wait_for("message", check=check, timeout=30)
+        except asyncio.TimeoutError:
+            await message.edit(content="Search cancelled, timed out.")
+            return
+
+        if confirm.content != '':
+            searchTerm = confirm.content
+            searchTerm = searchTerm.replace(' ','+')
+        
+        html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + searchTerm)
+        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
+
+def setup(bot: commands.Bot):
+    bot.add_cog(ytsearch(bot))
